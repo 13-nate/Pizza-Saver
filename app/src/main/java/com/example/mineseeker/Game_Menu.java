6ef15010b8894ba68db0bc09bd477e3676d011aa
Kevin Chen Su <kchensu@sfu.ca>
2020-02-07 19:20:27 -0800

    Main Menu, fixed a bug where it kept crashing the the app

--- app/src/main/java/com/example/mineseeker/Game_Menu.java
+++ app/src/main/java/com/example/mineseeker/Game_Menu.java
@@ -17,24 +17,14 @@ public class Game_Menu extends AppCompatActivity {
     private Button playGameButton;
     private Button optionButton;
     private ImageView explosionLogo;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game__menu);
         startAnimation();
 
-        helpButton = findViewById(R.id.help_button);
-        helpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Game_Menu.this, HelpMenuActivity.class);
-                startActivity(intent);
-            }
-        });
 
         playGameButton = findViewById(R.id.playGame_button);
-
         playGameButton.setOnClickListener(view -> {
             Intent intent = PlayGameActivity.makeIntentPlayGameActivity(Game_Menu.this);
             startActivity(intent);
@@ -47,9 +37,15 @@ public class Game_Menu extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(Game_Menu.this, OptionsActivity.class);
                 startActivity(intent);
-
             }
-
+        });
+        helpButton = findViewById(R.id.help_button);
+        helpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Game_Menu.this, HelpMenuActivity.class);
+                startActivity(intent);
+            }
         });
 
 
@@ -58,13 +54,20 @@ public class Game_Menu extends AppCompatActivity {
     private void startAnimation() {
 
         Animation menuAnimation = AnimationUtils.loadAnimation(this, R.anim.lefttoright);
+        Animation explosionLogoAnimation = AnimationUtils.loadAnimation(this,R.anim.explosion_logo);
+
+
+        explosionLogo = findViewById(R.id.explosionLogo_imageView);
         TextView menu = findViewById(R.id.mainMenu_textView);
         Button playButton = findViewById(R.id.playGame_button);
         Button optionButton = findViewById(R.id.options_button);
         Button helpButton = findViewById(R.id.help_button);
+
+
         menu.startAnimation(menuAnimation);
         playButton.startAnimation(menuAnimation);
         optionButton.startAnimation(menuAnimation);
         helpButton.startAnimation(menuAnimation);
+        explosionLogo.startAnimation(explosionLogoAnimation);
     }
 }
