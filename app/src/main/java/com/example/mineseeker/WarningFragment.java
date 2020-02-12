65c50cfe5e11307506d0389a526ebc7448fbe1a4
Kevin Chen Su <kchensu@sfu.ca>
2020-02-11 20:52:32 -0800

    added SharePreference between activities to save times played

---
+++ app/src/main/java/com/example/mineseeker/WarningFragment.java
@@ -0,0 +1,44 @@
+package com.example.mineseeker;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class WarningFragment extends AppCompatDialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.clear_warning_message,null );
+        SharedPreferences clearData = this.getActivity().getSharedPreferences("COUNT", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = clearData.edit();
+
+
+        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                editor.clear().commit();
+                Intent intent = Game_Menu.makeIntentGameMenuActivity(getActivity());
+                startActivity(intent);
+                getActivity().finish();
+            }
+        };
+
+        return new AlertDialog.Builder(getActivity()).setTitle("WARNING!")
+                .setView(v)
+                .setPositiveButton(android.R.string.ok, listener)
+                .create();
+    }
+}
